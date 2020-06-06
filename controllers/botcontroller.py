437edb533622e152a5ba6c453c025ce52e38bc70
firefly <firefly@fireflys-Air.home>
2020-06-06 00:21:03 -0400

    made pins stop and forward

--- controllers/botcontroller.py
+++ controllers/botcontroller.py
@@ -16,9 +16,17 @@ class BotController():
         message = busMessage.Message
         if message == "L":
             print("L")
-            set_pin(0, )
+            set_pin(0, 1)
+        elif message == "LS":
+            print("LS")
+            set_pin(0, 0)
         elif message == "R":
             print("R")
+            set_pin(0, 1)
+        elif message == "RS":
+            print("RS")
+            set_pin(0,0)
+        
 
 def createBotController():
     return BotController(createMqttConnection())
