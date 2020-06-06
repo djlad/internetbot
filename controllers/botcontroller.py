87be86e1ed9dd274e3a1272750c450deadfbec34
firefly <firefly@fireflys-Air.home>
2020-06-06 12:53:09 -0400

    allow setting pin numbers

--- controllers/botcontroller.py
+++ controllers/botcontroller.py
@@ -14,19 +14,20 @@ class BotController():
     
     def handleMqtt(self, busMessage):
         assert isinstance(busMessage, BusMessage)
-        message = busMessage.Message
+        message = busMessage.Message.split(" ")[0]
+        pinNumber = int(busMessage.Message.split(" ")[1])
         if message == "L":
             print("L")
-            self.pins.set_pin(0, 1)
+            self.pins.set_pin(pinNumber, 1)
         elif message == "LS":
             print("LS")
-            self.pins.set_pin(0, 0)
+            self.pins.set_pin(pinNumber, 0)
         elif message == "R":
             print("R")
-            self.pins.set_pin(0, 1)
+            self.pins.set_pin(pinNumber, 1)
         elif message == "RS":
             print("RS")
-            self.pins.set_pin(0, 0)
+            self.pins.set_pin(pinNumber, 0)
         
 
 def createBotController():
